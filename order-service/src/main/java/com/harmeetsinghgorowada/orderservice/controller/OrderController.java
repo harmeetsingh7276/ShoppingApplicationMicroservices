991e1aa9a54401332a70ca5fa70b695e8be9c0b8
Harmeet Singh Gorowada <harmeetsingh7276@gmail.com>
2023-07-24 12:17:19 +0530

    Added Order Service Create Order API

---
+++ order-service/src/main/java/com/harmeetsinghgorowada/orderservice/controller/OrderController.java
@@ -0,0 +1,20 @@
+package com.harmeetsinghgorowada.orderservice.controller;
+
+import com.harmeetsinghgorowada.orderservice.dto.OrderRequest;
+import com.harmeetsinghgorowada.orderservice.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/order")
+@RequiredArgsConstructor
+public class OrderController {
+    private final OrderService orderService;
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String placeOrder(@RequestBody OrderRequest orderRequest) {
+        orderService.placeOrder(orderRequest);
+        return "Order Placed";
+    }
+}
